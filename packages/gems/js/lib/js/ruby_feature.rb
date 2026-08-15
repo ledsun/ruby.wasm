f0b760d2f0341397e3a1c66f24a4afae900c21c1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2026-08-15 18:22:30 +0900

    Document Ruby feature filename normalization

--- packages/gems/js/lib/js/ruby_feature.rb
+++ packages/gems/js/lib/js/ruby_feature.rb
@@ -1,5 +1,7 @@
 module JS
   module RubyFeature
+    # Convert a require feature name to a Ruby source filename, appending the
+    # .rb extension when it is omitted.
     def self.filename_from(feature)
       feature = feature.to_s
       feature.end_with?(".rb") ? feature : "#{feature}.rb"
